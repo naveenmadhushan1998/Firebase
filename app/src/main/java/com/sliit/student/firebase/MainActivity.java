445a4e1aa33cce8e80307a50e37817120b43a00f
tdwic <wickramanayakatharindu@gmail.com>
2019-09-07 11:26:49 +0530

    Fire1

---
+++ app/src/main/java/com/sliit/student/firebase/MainActivity.java
@@ -0,0 +1,65 @@
+package com.sliit.student.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txt_id,txt_name,txt_address,txt_contact;
+    Button btn_save,btn_show,btn_update,btn_delete;
+    Student student;
+    DatabaseReference dbRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt_id = findViewById(R.id.txtId);
+        txt_name = findViewById(R.id.txtName);
+        txt_address = findViewById(R.id.txtAddress);
+        txt_contact = findViewById(R.id.txtContact);
+
+        btn_save = findViewById(R.id.btnSave);
+        btn_show = findViewById(R.id.btnShow);
+        btn_update = findViewById(R.id.btnUpdate);
+        btn_delete = findViewById(R.id.btnDelete);
+
+        student =new Student();
+
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbRef = FirebaseDatabase.getInstance().getReference().child("Student");
+
+                student.setId(txt_id.getText().toString().trim());
+                student.setName(txt_name.getText().toString().trim());
+                student.setAddress(txt_address.getText().toString().trim());
+                student.setContactnum(Integer.parseInt(txt_contact.getText().toString().trim()));
+
+                dbRef.push().setValue(student);
+                Toast.makeText(getApplicationContext(), "Adding Success",Toast.LENGTH_LONG).show();
+                clearData();
+            }
+        });
+    }
+
+    public void clearData(){
+        txt_id.setText("");
+        txt_name.setText("");
+        txt_address.setText("");
+        txt_contact.setText("");
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/sliit/student/firebase/Student.java
@@ -0,0 +1,43 @@
+package com.sliit.student.firebase;
+
+public class Student {
+    private String id;
+    private String name;
+    private String address;
+    private int contactnum;
+
+    public Student() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getContactnum() {
+        return contactnum;
+    }
+
+    public void setContactnum(int contactnum) {
+        this.contactnum = contactnum;
+    }
+}
